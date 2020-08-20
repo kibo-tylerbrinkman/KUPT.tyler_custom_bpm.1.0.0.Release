7f8a2953e96d14d4acf2155204f259abc07cff28
Shu Hao <Shu.Hao@kibocommerce.com>
2020-08-20 10:02:20 -0500

    add Digital process (#7)
    * add Digital process
    
    * update pom to 1.2035-SNAPSHOT

---
+++ src/main/java/com/kibocommerce/bpm/fulfillment/FulfillmentAction.java
@@ -0,0 +1,23 @@
+package com.kibocommerce.bpm.fulfillment;
+
+public enum FulfillmentAction {
+    AcceptShipment,
+    CancelShipment,
+    CancelItems,
+    CustomerCareShipment,
+    CustomerCareItems,
+    RejectShipment,
+    RejectItems,
+    ReassignShipment,
+    ReassignItems,
+    BackorderShipment,
+    BackorderItems,
+    TransferShipment,
+    TransferItems,
+    CompleteShipment,
+    FulfillItems,
+    ShipTransferShipment,
+    ReadyForPickup,
+    CapturePayment,
+    ;
+}
